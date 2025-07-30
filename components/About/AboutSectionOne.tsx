feb6b04dc56dc6f9d12696cc2f2413c93428073c
Ebenezer Seyoum <ebenezer1621@gmail.com>
2025-07-30 14:24:55 +0300

    initial commit

--- components/About/AboutSectionOne.tsx
+++ components/About/AboutSectionOne.tsx
@@ -23,30 +23,30 @@ const AboutSectionOne = () => {
           className="w-full max-w-[96rem] mx-auto bg-white text-green-900 rounded-lg border border-gray-200 shadow-[0_10px_30px_rgba(0,0,0,0.1)] px-4 sm:px-6 md:px-8 lg:px-20 py-10 md:py-14 lg:py-16 flex flex-col-reverse lg:flex-row items-center gap-10 sm:gap-12"
         >
           {/* Text Content */}
-        <div className="w-full lg:w-8/12 max-w-[850px] text-left bg-slate-300 p-4 sm:p-6 md:p-8 rounded-lg">
-  <div className="space-y-4 sm:space-y-6 text-base sm:text-lg md:text-xl leading-7 sm:leading-8 md:leading-9 font-normal text-green-900 font-serif text-left sm:text-justify">
+       <div className="w-full lg:w-8/12 max-w-[850px] text-left bg-slate-300 p-4 sm:p-6 md:p-8 rounded-lg">
+  <div className="space-y-4 sm:space-y-6 text-base sm:text-lg md:text-xl leading-7 sm:leading-8 md:leading-9 font-normal text-green-900 font-serif text-left md:text-justify">
     <h1 className="text-3xl sm:text-4xl font-extrabold mb-4 text-dark font-serif">About Me</h1>
-    
+
     <p>
       I recently graduated with a degree in Information Technology and have gained practical experience
       in full-stack development, mobile app development, networking, and cybersecurity. I’m comfortable
       working with Python, PHP, React.js, Node.js, and troubleshooting networks, and I’ve also completed
       training in IoT and cybersecurity.
     </p>
-    
+
     <p>
       During university, I worked on a court information management system project where I built a
       full-stack application to help improve case tracking and data access for legal institutions. I also had
       the chance to intern at East Africa Lion Brands, where I supported system maintenance, IT support,
       and security tasks.
     </p>
-    
+
     <p>
       I enjoy working with databases, Git, and Linux, and take pride in writing clean, efficient code that
       solves real problems. I’m always eager to learn new skills, adapt to challenges, and contribute
       meaningfully to any team I join.
     </p>
-    
+
     <p>
       In addition to development, I have a strong interest in DevOps practices, including version control
       with Git, automation, CI/CD pipelines, Linux system administration, and containerization tools like
@@ -55,6 +55,7 @@ const AboutSectionOne = () => {
     </p>
   </div>
 </div>
+
 
 
           {/* Profile Image */}
